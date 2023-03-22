102744f1546a83da66129558110be50091bddb3f
wanghao75 <86595007+wanghao75@users.noreply.github.com>
2023-03-22 11:31:17 +0800

    Update patch2pr.py

--- patch2pr/patch2pr.py
+++ patch2pr/patch2pr.py
@@ -227,7 +227,7 @@ def get_email_content_sender_and_covert_to_pr_body(ser_id):
     # check if getmail has received all patches-email when script has been started
     cur.execute("SELECT id from patchwork_patch where series_id={}".format(ser_id))
     number_of_patches = cur.fetchall()
-    
+
     if len(number_of_patches) != all_patches_in_series:
         return "", "", "", ""
 
